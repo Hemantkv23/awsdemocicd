ce22de646365304691a3bab7f439215eca7d9373
Hemant <hemant333042@gmail.com>
2025-01-09 19:09:37 +0530

    changed the region to ap-south-1

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+import { pipeline } from 'stream';
 
 export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    //AWS CICD
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoawspipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', {
+
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub('Hemantkv23/awsdemocicd', 'main' ),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npm cdk synth',
+        ],
+      }),
+    });
+
   }
 }

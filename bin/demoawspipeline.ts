8eb71b9bcdc59a91065296ac0df0c64337dc38df
Hemant <hemant333042@gmail.com>
2025-02-02 15:59:20 +0530

    initial commit

---
+++ bin/demoawspipeline.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
+
+const app = new cdk.App();
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  
+  env: { account: '242201272000', region: 'ap-south-1' },
+
+});
---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    //AWS CI-CD Pipeline
+
+    const democicdpipeline = new CodePipeline(this, 'demopipeline',
+      {
+        synth: new ShellStep('Synth', {
+          input: CodePipelineSource.gitHub('Hemantkv23/awsdemocicd', 'main'),
+          commands: [
+            'npm ci',
+            'npm run build',
+            'npx cdk synth',
+          ],
+        }),
+      });
+  }
+}

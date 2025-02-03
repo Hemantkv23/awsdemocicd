a54fb8ff1dfcf52703865b4f6da8441400be78e0
Hemant <hemant333042@gmail.com>
2025-02-03 18:03:25 +0530

    final commit

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,23 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { SecretValue } from 'aws-cdk-lib/core';
+import { pipelineAppStage } from './demoawspipeline-app-stack';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    //AWS CI-CD Pipeline
+    // Get GitHub token from AWS Secrets Manager
+    const gitHubAccessToken = SecretValue.secretsManager('myGitHubToken3');
 
-    const democicdpipeline = new CodePipeline(this, 'demopipeline',
-      {
-        synth: new ShellStep('Synth', {
-          input: CodePipelineSource.gitHub('Hemantkv23/awsdemocicd', 'main'),
-          commands: [
-            'npm ci',
-            'npm run build',
-            'npx cdk synth',
-          ],
+    // AWS CI/CD Pipeline
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('Hemantkv23/awsdemocicd', 'main', {
+          authentication: gitHubAccessToken, // Pass the token here
         }),
-      });
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+
+    const testingStage = democicdpipeline.addStage(new pipelineAppStage(this, 'test', {
+      env: {account: '242201272000', region: 'ap-south-1'}
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new pipelineAppStage(this, 'prod', {
+      env: {account: '242201272000', region: 'ap-south-1'}
+    }));
+
+
   }
 }
